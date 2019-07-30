603cc956764ddaa71953987d70df6b7462e69b88
Jannik Zschiesche <jz@becklyn.com>
2019-07-30 23:33:04 +0200

    Add basic implementation

---
+++ src/Controller/DumpRoutesController.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\JavaScriptRouting\Controller;
+
+use Becklyn\JavaScriptRouting\Extractor\RoutesExtractor;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DumpRoutesController extends AbstractController
+{
+    /**
+     * @param string $locale
+     */
+    public function dump (
+        RoutesExtractor $extractor,
+        ParameterBagInterface $parameters,
+        Request $request,
+        string $locale
+    ) : Response
+    {
+        $isDebug = $parameters->get("kernel.debug");
+        $collection = $extractor->extract($locale, !$isDebug);
+        $json = \addslashes($collection->getJson());
+
+        $response = new JsonResponse(
+            // use JSON parse and a string here, as it is way faster than parsing JavaScript in the browser.
+            "JSON.parse('{$json}')",
+            200,
+            [
+                // prevent magic byte insertion
+                "X-Content-Type-Options" => "nosniff",
+            ],
+            true
+        );
+
+        $response->setCallback("window.RouterInit.init");
+
+        if (!$isDebug)
+        {
+            $response->setEtag($collection->getHash());
+            $response->isNotModified($request);
+        }
+
+        return $response;
+    }
+}
---
+++ src/Twig/JavaScriptRoutesTwigExtension.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\JavaScriptRouting\Twig;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class JavaScriptRoutesTwigExtension extends AbstractExtension implements ServiceSubscriberInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $locator;
+
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct (ContainerInterface $locator, RequestStack $requestStack)
+    {
+        $this->locator = $locator;
+        $this->requestStack = $requestStack;
+    }
+
+
+    /**
+     * @param string|null $locale
+     *
+     * @return string
+     */
+    public function renderInit (?string $locale = null) : string
+    {
+        if (null === $locale)
+        {
+            $request = $this->requestStack->getMasterRequest();
+
+            if (null === $request)
+            {
+                return "<-- Can't embed, because no locale given -->";
+            }
+
+            $locale = $request->getLocale();
+        }
+
+        return $this->locator->get(Environment::class)->render("@BecklynJavaScriptRouting/init.html.twig", [
+            "locale" => $locale,
+        ]);
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function getFunctions ()
+    {
+        return [
+            new TwigFunction("javascript_routes_init", [$this, "renderInit"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedServices ()
+    {
+        return [
+            Environment::class,
+        ];
+    }
+}
